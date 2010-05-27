5402303d0c41e43b79f73bff13dbd37381a8ad72
Harald Pehl <harald.pehl@gmail.com>
2010-05-27 20:21:42 +0000

    Updated to Restlet 2.0-RC3

--- src/name/pehl/tire/client/project/NewProjectEventHandler.java
+++
@@ -1,8 +0,0 @@
-package name.pehl.tire.client.project;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface NewProjectEventHandler extends EventHandler
-{
-    void onNewProject(NewProjectEvent event);
-}
