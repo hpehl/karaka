fa6e9930c99520880b5d2d5416bb9338867f16b5
Harald Pehl <harald.pehl@gmail.com>
2010-09-17 11:12:34 +0000

    Started project usecase

---
+++ app/src/main/java/name/pehl/tire/client/project/Project.java
@@ -0,0 +1,19 @@
+package name.pehl.tire.client.project;
+
+import name.pehl.piriti.client.json.JsonField;
+
+import com.gwtplatform.annotation.GenDto;
+
+/**
+ * @author $Author$
+ * @version $Date$ $Revision$
+ */
+@GenDto
+public class Project
+{
+    Long id;
+
+    String name;
+
+    String description;
+}
