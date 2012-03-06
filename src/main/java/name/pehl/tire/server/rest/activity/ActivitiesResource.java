85d3228590edb9caa473afaf322512fe1b0ddf4c
Harald Pehl <harald.pehl@gmail.com>
2012-03-06 16:19:52 +0000

    Added code to test JAXB with Jackson to generate JSON

--- src/main/java/name/pehl/tire/server/rest/activity/ActivitiesResource.java
+++ src/main/java/name/pehl/tire/server/rest/activity/ActivitiesResource.java
@@ -12,6 +12,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import name.pehl.tire.server.dao.ActivityDao;
 import name.pehl.tire.server.model.ActivitiesGenerator;
@@ -63,7 +64,7 @@ public class ActivitiesResource
 
     @GET
     @Path("{year:[0-9]{4}}/{month:[0-9]{2}}")
-    @Produces("application/json")
+    @Produces(MediaType.APPLICATION_JSON)
     public Activities getActivities(@PathParam("year") int year, @PathParam("month") int month)
     {
         ActivityParameters ap = new ActivityParameters().parse(getRequestAttributes());
